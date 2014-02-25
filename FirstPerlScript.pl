237426566d1cd56a6f23b4e9945b685943d9a5f7
i3-Arch <jamarlow410@st.nashcc.edu>
2014-02-25 16:18:02 -0500

    1st Perl Script-Update

--- FirstPerlScript.pl
+++ FirstPerlScript.pl
@@ -2,11 +2,14 @@
 use strict;
 use warnings;
 	print "What is your name? ";
-		my $name = <STDIN>;
+
+my $name = <STDIN>;
+
 	print "Hello, $name!\n";
 	print "What is your age? ";
-		my $age = <STDIN>;
-	print "Your age is $age!\n";
 
-print "So you are ", ($name)  ;
-print "And your age is ", ($age)  ;
+my $age = <STDIN>;
+
+	print "Your age is $age!\n";
+	print "So you are ", ($name)  ;
+	print "And your age is ", ($age)  ;
