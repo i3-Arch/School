440c880afaded21705f9b0a36c799ed0005f385a
i3-Arch <jamarlow410@st.nashcc.edu>
2014-02-25 19:24:44 -0500

    update

--- FirstPerlScript.pl
+++ FirstPerlScript.pl
@@ -1,15 +1,30 @@
 #!/usr/bin/perl
+# By Jeremy Marlow
+# My First Perl Script
 use strict;
 use warnings;
+use Term::ANSIScreen qw(cls);
+
+my $clear = cls();
+
+	print $clear ;
 	print "What is your name? ";
 
 my $name = <STDIN>;
 
-	print "Hello, $name!\n";
-	print "What is your age? ";
+	print "Sup, $name \n" ;
+
+	print "So how old are you $name \n ";
 
 my $age = <STDIN>;
 
-	print "Your age is $age!\n";
-	print "So you are ", ($name)  ;
-	print "And your age is ", ($age)  ;
+if ($age >= 18)		{ 
+	
+	print $clear ;
+	print "can i haz sex wif you $name \n" ;
+}
+
+else { 
+
+	print " y u no 18, why you $age \n" ;
+}
