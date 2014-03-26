3d45222d90ce1675f4a4d74737c25506af3c9e52
i3-Arch <jamarlow410@st.nashcc.edu>
2014-03-25 23:45:21 -0400

    CPP

---
+++ loop.cpp
@@ -0,0 +1,16 @@
+// custom countdown using while loop
+//  not mine <<    ||  using as reference
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+  int n = 10;
+
+  while (n>0) {
+    cout << n << ", ";
+    --n;
+  }
+
+  cout << " This is a loop ";
+}
