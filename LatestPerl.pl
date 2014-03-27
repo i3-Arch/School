4d52d9d35255f398e737dc18b32f84b8d5980281
i3-Arch <jamarlow410@st.nashcc.edu>
2014-03-27 18:49:57 -0400

    update

--- LatestPerl.pl
+++ LatestPerl.pl
@@ -1,16 +1,18 @@
 #!/usr/bin/perl
 # i3-Arch
 ################## 
+use strict;
+use warnings;
 use Term::ANSIColor;
 print `clear`;
 print color("red"), "This was created by i3-Arch ! \n", color("reset");
 print color("green"), " Just A Simple Perl Script \n", color("reset");
 print " Your Architecture is ";
-$arch = `uname -m \n` ;
-print " $arch \n " ;  
+my ($arch) = `uname -m \n` ;
+print (" $arch \n ") ;  
 
 print color("yellow"), " Your IP address is ", color("reset"), color("bold blue");
-$ipaddress = system ("curl wtfismyip.com/text 2> /dev/null \n"), color("reset");
+my ($ipaddress) = system ("curl wtfismyip.com/text 2> /dev/null \n"), color("reset");
 
 print color("green"), " \n Pick a Number \n \n ", color("reset");
 print color("magenta"), " ANY NUMBER \n \n ", color("reset");
@@ -39,5 +41,6 @@ else {
 
 print color("blue"), "  \n Thanks for using this script  \n", color("reset");
 print color("red"), " \n Here is the output of netstat -tupNa  \n ", color("reset");
-$HUE = `\n netstat -tupNa > tst023 ; cat tst023 ; rm tst023 2> /dev/null \n`;
-print color("green"), $HUE , color("reset");
+print color("green");
+my ($HUE) = system ("\n netstat -tupNa > tst023 ; cat tst023 ; rm tst023 2> /dev/null \n") ;
+print color("reset");
