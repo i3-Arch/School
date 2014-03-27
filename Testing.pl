2beb9900d37543d207e1b6444f57194bd0ed1d9a
i3-Arch <jamarlow410@st.nashcc.edu>
2014-03-27 12:23:11 -0400

    update

--- Testing.pl
+++ Testing.pl
@@ -4,13 +4,28 @@
 print " Your Architecture is ";
 $arch = system ("uname -m");
 print " $arch \n " ;  
+
 print " Your IP address is ";
 $ipaddress = system ("curl wtfismyip.com/text 2> /dev/null ");
+
 print " Pick a Number  \n";
 print " ANY NUMBER  \n ";
+
 my $QUESTION = <STDIN>;
+
 print " So you picked $QUESTION \n ";
-if ($QUESTION >= 9001) { print " ITS OVAR NINE THOUSAND ! ";}
-			elsif ($QUESTION == 0) { print " What The Fuck \n  "; print " You could pick any number \n "; print " AND YOU FUCKING PICKED 0 \n ";}
-		unless ($QUESTION > 0) { print " Nice Try Faggot \n "; print " Cant fool me bro \n "; }
-else { print " Fuck You. Have A Nice Day ";}
+
+if ($QUESTION >= 9001) { 
+	
+		print " ITS OVAR NINE THOUSAND ! "; }
+			
+			elsif ($QUESTION == 0) { 
+		
+			print " What The Fuck \n  "; print " You could pick any number \n "; print " AND YOU FUCKING PICKED 0 \n ";}
+		
+		unless ($QUESTION > 0) { 
+		
+			print " Nice Try Faggot \n "; print " Cant fool me bro \n "; }
+else { 
+	
+	print " Fuck You. Have A Nice Day ";}
