91e1e110c9391ab45f97946dd8582d3fdd4ba3c0
i3-Arch <i3_Arch@archlinux.info>
2014-09-05 07:44:12 -0400

    Update

--- C/agebro.c
+++ C/agebro.c
@@ -1,20 +1,17 @@
+/*   This is just a lame script I made in C... 
+ *
+ *			Forgive me for being a newb and creating un-nessasary function ( submain )
+ *				-- i3-Arch --
+ */
+
 #include <stdio.h>
 
-int main()   
+int full()   
 {
+	
 	int YourAge;
 
 	system( "clear" );
-	
-	/*printf("\x1b[31mThis is red text\x1b[0m\n");
-	printf("\x1b[32mThis is green text\x1b[0m\n");
-	printf("\x1b[33mThis is yellow text\x1b[0m\n");
-	printf("\x1b[34mThis is blue text\x1b[0m\n");
-	printf("\x1b[35mThis is purple text\x1b[0m\n");
-	printf("\x1b[36mThis is orange text\x1b[0m\n");
-	printf("\x1b[37mThis is white text\x1b[0m\n");
-	*/
-	
 	printf( "\x1b[37m\n\nPlease enter your current age: \x1b[0m" );
 	scanf( "%d", &YourAge );
 	if ( YourAge < 12 && YourAge > 0 ) {
@@ -37,20 +34,37 @@ int main()
 	}
 	else {
 	printf( "\x1b[36\nEhh, I guess you're my elder? Since you're so old...\n\x1b[0m" );
-	}
+	}	
 	
-	subfunc();
 	return 0;
 }
 
+int submain()
+{
+	char enter = 0;
+	printf("\x1b[34m\n\nPlease Press enter to continue\n\n \x1b[0m");
+	while (enter != '\r' && enter != '\n') { enter = getchar(); }
+	
+	return 0;
+}
+
+
 int subfunc()
 {
-	char ColorBro[10];
 
-	printf( "\x1b[37m\n\nPlease Enter Your Favorite Color: \x1b[0m" );
+	char ColorBro[10];
+	
+	printf( "\x1b[37m\n\n Enter Your Favorite Color: \x1b[0m" );
 	scanf( "%s", &ColorBro );
 	printf( "\x1b[34m\n\nYour Favorite Color is: \x1b[0m" );
 	printf( "\x1b[37m%s\n\n\x1b[0m", ColorBro );
-	
+
 	return 0;
+}
+
+int main()
+{
+	submain();	
+	full();
+	subfunc();
 }
