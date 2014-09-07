3314a0e0db663d23de1d5171a093f7451433d982
i3-Arch <i3_Arch@archlinux.info>
2014-09-07 01:32:44 -0400

    Example 1

--- Python/test.py
+++ Python/test.py
@@ -1,6 +1,9 @@
-#!/usr/bin/perl
+#!/usr/bin/python2
 #
 #   Example
 
+import os
+os.system('clear')
+print "\n\nHello World\n\n"
 name = raw_input('\nWhat is your name?\n\n Name: ')
 print 'Hi, %s.' %name
