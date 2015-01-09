f1f5fbf47a0cbd28015b188a82c605b33df11060
i3-Arch <i3_Arch@archlinux.info>
2015-01-09 03:21:15 -0500

    Update

--- Perl/LatestPerl.pl
+++ Perl/LatestPerl.pl
@@ -1,48 +1,72 @@
 #!/usr/bin/perl
-# i3-Arch
-################## 
+###########################################
+##			|						     ##
+## i3-Arch  |  Just A Simple PERL Script ##
+##			|		  #Newb2Perl		 ##
+###########################################
+
+#use warnings;
 use strict;
-use warnings;
 use Term::ANSIColor;
-print `clear`;
-print color("red"), "This was created by i3-Arch ! \n", color("reset");
-print color("green"), " Just A Simple Perl Script \n", color("reset");
-print " \n\nYour Architecture is ";
-my ($arch) = `uname -m \n` ;
-print (" $arch \n ") ;  
 
-print color("yellow"), " Your IP address is ", color("reset"), color("bold blue");
-my ($ipaddress) = system ("curl wtfismyip.com/text 2> /dev/null \n"), color("reset");
+sub Greetz{
+	print `clear`;
+	print color("yellow"), "\n\n  This was created by ----> ", color("white"), "i3-Arch \n", color("reset");
+	print color("blue"), "\n\n  Sup TrewChainz \n\n";
+}
 
-print color("green"), " \n Pick a Number \n \n ", color("reset");
-print color("magenta"), " ANY NUMBER \n \n ", color("reset");
+sub Archit{
+	print color("green"), "\n\n  Your Architecture is ---> ";
+	my ($arch) = `uname -m \n` ;
+	print color("white"), (" $arch \n ");  
+}
 
-my $QUESTION = <STDIN>;
+sub IPadd{
+	#print color("green"), "\n\n Would you like to see your external IP address? \n\n";
+	#print color("yellow"), "\n Choose 'Y' or 'N' \n\n";        ## Still playin with this
+	#print color("white"), "\n Choice: ";
+	#my $Answer = <STDIN>;
+	#if ($Answer eq 'Y' or 'y') {
+	print color("yellow"), " Your external IP address is ", color("reset"), color("bold blue");
+	my ($ipaddress) = system ("curl wtfismyip.com/text 2> /dev/null \n"), color("reset");
+	#}else {
+	#	print color("yellow"), "\n\n Fine then...\n\n";}
+}
 
-print color("blue"), " \n So you picked $QUESTION \n ", color("reset");
+sub Numba{
+	print color("green"), "\n  Pick a Number \n\n", color("reset");
+	print color("blue"), "  ANY NUMBER \n\n", color("reset");
+	print color("white"), "\n  Number: ";
+	my $QUESTION = <STDIN>;
+	print color("blue"), " \n  So you picked $QUESTION \n ", color("reset");
+	if ($QUESTION >= 9001) { 
+			print color("red"), "\n\n  ITS OVAR NINE THOUSAND ! \n ", color("reset"); }
+	elsif ($QUESTION <= 0) { 	
+			print color("green"), "  What The Fuck \n\n", color("reset"); 
+			print color("white"), "  You could pick any number \n\n", color("reset"); 
+			print color("yellow"), "  AND YOU FUCKING PICKED 0 or Random symbol(s)/letters \n\n",color("reset") ;}
+	else {
+		print color("white"), "\n\n  Ok?  I'm guessing that's a special number to you ?!?!?!? \n\n";}
+}
 
-if ($QUESTION >= 9001) { 
-	
-			print color("red"), "ITS OVAR NINE THOUSAND ! \n ", color("reset"); }
-			
-elsif ($QUESTION == 0) { 
-		
-			print color("green"), " What The Fuck \n \n", color("reset"); 
-			print color("white"), " You could pick any number \n \n ", color("reset"); 
-			print color("red"), " AND YOU FUCKING PICKED 0 \n \n",color("reset") ;}
-		
-unless ($QUESTION >= -1) { 
-		
-			print color("yellow"), " Nice Try Faggot \n \n ", color("reset"); 
-			print color("red"), " Cant fool me bro \n \n ", color("reset") ;}
-else { 
-	
-print color("green"), " \n Fuck You. Have A Nice Day \n ", color("reset");}
+sub Stat{
+	print color("white"), "\n  Here is the output of ", color("yellow"), "netstat -tupNa  \n", color("reset");
+	print color("green");
+	my ($HUE) = system ("\n  netstat -tupNa > tst023 ; cat tst023 ; rm tst023 2> /dev/null \n");
+	print color("reset");
+}	
 
-print color("blue"), "  \n #SWAG  #YOLO  #IDGAF  \n", color("reset");
+sub Pause{
+	local( $| ) = ( 1 );
+	print color("white"), " \n  Press", color("red"), "<Enter>", color("white"), "to stop script: \n\n", color("reset");
+	my $resp = <STDIN>;	
+}
 
-print color("red"), " \n Here is the output of netstat -tupNa  \n ", color("reset");
-print color("green");
-my ($HUE) = system ("\n netstat -tupNa > tst023 ; cat tst023 ; rm tst023 2> /dev/null \n") ;
-print color("reset");
-die "\n\n Thanks for using this script ! \n\n";
+Pause
+Stat
+Numba
+IPadd
+Archit
+Greetz
+
+#EOF
