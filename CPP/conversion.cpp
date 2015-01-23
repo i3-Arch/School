6dcaf44f1b24465a462519c728011664143c8aa8
i3-Arch <i3_Arch@archlinux.info>
2015-01-22 20:10:06 -0500

    Bored

---
+++ CPP/conversion.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stdio.h>
+
+int main() {
+
+	const double BRITISH_POUNDS	= 0.6318;
+	const double MEXICAN_PESOS	= 12.8863;
+	const double JAPANESE_YEN	= 82.34;
+	double amtBritish			= 0.0;
+	double amtMexican			= 0.0;
+	double amtJapanese			= 0.0;
+	double americanDollars		= 0.0;
+
+
+	printf("Enter Amount of American Dollars to convert: ");
+	std::cin >> americanDollars;
+
+	amtBritish	=	americanDollars * BRITISH_POUNDS;
+	amtMexican	=	americanDollars * MEXICAN_PESOS;
+	amtJapanese	=	americanDollars * JAPANESE_YEN;
+
+	std::cout << "American Dollars: $ " << americanDollars << std::endl;
+	std::cout << "British Pounds: £ " << amtBritish << std::endl;
+	std::cout << "Mexican Pesos: $ " << amtMexican << std::endl;
+	std::cout << "Japanese Yen: ¥ " << amtJapanese << std::endl;
+
+	return 0;
+
+}
