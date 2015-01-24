bd5d772700a896a3ed7ce12c699d9e76e96fa2b8
NunYaBidness <jamarlow410@st.nashcc.edu>
2015-01-24 14:32:57 -0500

    Update First.cpp

--- CPP/First.cpp
+++ CPP/First.cpp
@@ -4,15 +4,13 @@ using namespace std;
 
 int main ()
 {
-	cout << "Sup." << endl;
+    cout << "Sup." << endl;
     cout << " First CPP script " << endl;
 
-		int n =  10;
-
-			while (n>0) {
-				cout << n << ", ";
-				--n;
-			}
-									
+    int n =  10;
+     while (n>0) {
+	cout << n << ", ";
+	--n;
+     }
 	return 0;
 }
