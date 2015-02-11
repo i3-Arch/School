70e03747aefbb528a69e0578bfdf26663daf98b5
NunYaBidness <jamarlow410@st.nashcc.edu>
2015-02-10 19:12:13 -0500

    OCD edit

--- CPP/exam.cpp
+++ CPP/exam.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
 
-	int candyBars			= 	0;  // Candy bars sold
+	int candyBars		=   0;  // Candy bars sold
 	const double clubProfit = .75;  // Profit each candy bar
 	const double studentGov	= .10;  // Student government percentage
 	double studentGovAmt	= 0.0;  // Student government amount/total
@@ -17,19 +17,15 @@ int main() {
 	cout << "##                                    ##\n";
 	cout << "########################################\n\n";
 	
-	
 	cout << "How many candy bars sold?" << endl;
 	cout << "Candy Bars Sold: ";
 	cin >> candyBars;
-
 	
-    static_cast <double> (studentGovAmt   = (candyBars * clubProfit) * studentGov);
+    	static_cast <double> (studentGovAmt   = (candyBars * clubProfit) * studentGov);
 	static_cast <double> (computerClubAmt = (candyBars * clubProfit) - studentGovAmt);
 	
-
 	cout << "Total Amount given to SGA: $" << studentGovAmt << endl;
 	cout << "Total Amount for Computer Club: $" << computerClubAmt << endl;
-
 
 	return 0;
 }
