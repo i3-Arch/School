9e80a8bb41604360198932df7dd5dd3b1c941bb1
i3-Arch <i3_Arch@archlinux.info>
2015-04-10 22:12:41 -0400

    huehuehue

---
+++ CPP/MISC/lol-test.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main() {
+
+
+	char yesNo = ' ';
+
+	
+	cout << "\n\n Is Arch Linux Master Race? \n" << endl;
+	cout << "\n\n  [Y/N] \n\n" << endl;
+	cout << "\n Choice: ";
+	
+	cin >> yesNo;
+	
+	do
+	{
+		if(yesNo == 'N' || yesNo == 'n')
+		{
+			for(int testA; testA != 9009; testA += 1001)
+			{
+				cout << "\n\n ARCH LINUX IS MASTER RACE \n\n" << endl;
+				//cout << "value of testA: " << testA << endl;
+				if(testA > 9000)
+					testA = 9009;
+			}
+			cout << "\n\n ==> Now you know that Arch Linux is in fact Master Race <==\n\n" << endl;
+			yesNo = 'Y';
+		}
+	
+		else if(yesNo == 'Y' || yesNo == 'y')
+		{
+			cout << "\n Swag, You Know The Trufax.... \n\n" << endl;
+			cout << "\n We should chill bro \n " << endl;
+		}
+		else
+		{
+			
+			cout << "\n\n\n\n\n\n\n\n ** [ Valid Options Are Y or N ] **\n\n\n\n\n\n\n\n\n\n\n\n\n" << endl;
+			cout << "\n\n Is Arch Linux Master Race? \n" << endl;
+	              	cout  << "\n\n  [Y/N] \n\n" << endl;
+        	      	cout << "\n Choice: ";
+			cin >> yesNo;
+		}
+
+	
+	}while(yesNo != 'Y' && yesNo != 'y');
+	
+
+	return 0;	
+}
