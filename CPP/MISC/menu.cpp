a01b6fb3aedec9f2eed7820c1b82e08b19580b20
i3-Arch <i3_Arch@archlinux.info>
2015-05-01 00:01:40 -0400

    update

--- CPP/MISC/menu.cpp
+++ CPP/MISC/menu.cpp
@@ -1,50 +1,57 @@
 #include <iostream>
 
-using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
 
-int main() {
+int main() 
+{
 
 	int myChoice;
 	bool myTest = true;
 
 
-	while (myTest != false){
+	while (myTest != false)
+	{
 
-		cout << "**********************************\n";
-		cout << "**     [1]  Print Dumb Stuff    **\n";
-		cout << "**     [2]  More Stuff          **\n";
-		cout << "**     [3]  MOAR STUFF          **\n";
-		cout << "**     [4]  Exit This Script    **\n";
-		cout << "**********************************\n";
-		cout << "\n\n\n   ==> Choice: ";
+		cout << "**********************************\n"
+		     << "**     [1]  Print Dumb Stuff    **\n"
+		     << "**     [2]  More Stuff          **\n"
+		     << "**     [3]  MOAR STUFF          **\n"
+		     << "**     [4]  Exit This Script    **\n"
+		     << "**********************************\n"
+		     << "\n\n\n   ==> Choice: ";
 
 		cin >> myChoice; 
 
-	switch (myChoice){
-	case 1:
-		cout << "\n    Doin dumb print\n\n";
-	// code
-		break;
-	case 2:
-		cout << "\n     More....\n\n";
-	// code
-		break;
-	case 3:
-		cout << "\n     MAAAAOOOORRRR\n\n";
-	// code
-		break;
-	case 4:
-		cout << "See you later.\n\n";
-		myTest = false;
-		break;
-	default:
-		cout << "\n\n  *** Not a Valid Choice ***\n\n";
-		cout << "\n\n   ==>  Choose Again  <==\n\n";
-		cin.clear();   // Entering a letter without 
-		cin.get();     // these two lines it will loop until ctrl+c
-		break;
-        }
+		switch (myChoice)
+		{
+			case 1:
+				cout << "\n    Doin dumb print\n\n";
+			break;
+		
+			case 2:
+				cout << "\n     More....\n\n";
+			break;
+	
+			case 3:
+				cout << "\n     MAAAAOOOORRRR\n\n";
+			break;
+	
+			case 4:
+				cout << "See you later.\n\n";
+				myTest = false;
+			break;
+	
+			default:
+				cout << "\n\n  *** Not a Valid Choice ***\n\n";
+				cout << "\n\n   ==>  Choose Again  <==\n\n";
+				cin.clear();   // Entering a letter without 
+				cin.get();     // these two lines it will loop until ctrl+c
+			break;
+        	}
 
-    }
+    	}
+	
 	return 0;
 }
