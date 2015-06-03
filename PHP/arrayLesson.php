a598eb3d491e465baaf418a7cdae163d01f1b69c
i3-Arch <i3_Arch@archlinux.info>
2015-06-03 02:52:46 -0400

    Bored

---
+++ PHP/arrayLesson.php
@@ -0,0 +1,11 @@
+<?php
+
+$even_numbers = [2,4,6,8,10];
+$male_names = ["Jake", "Eric", "John"];
+$female_names = ["Jessica", "Beth", "Sandra"];
+
+$names = array_merge($male_names, $female_names);
+print_r($even_numbers);
+print_r($names);
+
+?>
---
+++ PHP/varsLesson.php
@@ -0,0 +1,9 @@
+<?php
+$name = "JohnDoe";
+$age = 1337;
+echo "Hello $name. You are $age years old.\n";
+$x = 1;
+$y = 2;
+$sum = $x + $y;
+echo "The sum of $x and $y is $sum."
+?>
