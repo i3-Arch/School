4dfe779d35b4e3d7dcf27ee3847dcdc1619450a8
i3-Arch <i3_arch@archlinux.info>
2016-06-07 19:02:16 -0400

    Added Slope Function

--- CPP/Calculator-Project/Main.cpp
+++ CPP/Calculator-Project/Main.cpp
@@ -5,13 +5,15 @@ int main()
 
 	char myChoice = ' ';
 
-	cout << "\n  ####################" << endl
-	     << "  ##                ##" << endl
-	     << "  ##  [1] Add       ##" << endl
-	     << "  ##  [2] Multiply  ##" << endl
-	     << "  ##  [3] Exit      ##" << endl
-	     << "  ##                ##" << endl
-	     << "  ####################" << endl
+	cout << "\n  ######################" << endl
+	     << "  ##                  ##" << endl
+	     << "  ##  [1] Add         ##" << endl
+	     << "  ##  [2] Multiply    ##" << endl
+	     << "  ##  [3] Subtract    ##" << endl
+		 << "  ##  [4] Find Slope  ##" << endl
+		 << "  ##  [5] Exit        ##" << endl
+	     << "  ##                  ##" << endl
+	     << "  ######################" << endl
 	     << "\n\n Choice: ";
 	cin >> myChoice;
 
@@ -27,19 +29,35 @@ int main()
 			multiMe();
 			myChoice = ' ';
 		}
+		if(myChoice == '3')
+		{
+			subMe();
+			myChoice = ' ';
+		}
+		if(myChoice == '4')
+		{
+			slopeMe();
+			myChoice = ' ';
+		}
+		if(myChoice == '5')
+		{
+			cout << " Bye! " << endl;
+		}
 		else
 		{
-			cout << "\n  ####################" << endl
-                             << "  ##                ##" << endl
-			     << "  ##  [1] Add       ##" << endl
-			     << "  ##  [2] Multiply  ##" << endl
-        		     << "  ##  [3] Exit      ##" << endl
-                             << "  ##                ##" << endl
-			     << "  ####################" << endl
-             		     << "\n\n Choice: ";
+			cout << "\n  ######################" << endl
+                 << "  ##                  ##" << endl
+			     << "  ##  [1] Add         ##" << endl
+			     << "  ##  [2] Multiply    ##" << endl
+        		 << "  ##  [3] Subtract    ##" << endl
+				 << "  ##  [4] Find Slope  ##" << endl
+				 << "  ##  [5] Exit        ##" << endl
+                 << "  ##                  ##" << endl
+			     << "  ######################" << endl
+             	 << "\n\n Choice: ";
       		        cin >> myChoice;
 		}
-	}while(myChoice != '3');
+	}while(myChoice != '5');
 
 	return 0;
 }
--- CPP/Calculator-Project/head.h
+++ CPP/Calculator-Project/head.h
@@ -42,9 +42,65 @@ void multiMe()
 	cin >> num2;
 
 	myProduct = num1 * num2;
-
-	cout << "\n The Product of " << num1 << " and " << num2
+	
+	cout << fixed << setprecision(2)
+	     << "\n The Product of " << num1 << " and " << num2
 	     << " is " << myProduct << endl << endl
 	     << num1 << " x " << num2 << " = " << myProduct << endl;
 	    
+}
+
+void subMe()
+{
+	double num1		= 0.0;
+	double num2		= 0.0;
+	double myDiff	= 0.0;
+
+	cout << "\n Enter 2 Numbers to subtract \n"
+		 << "\n Number 1: ";
+		 cin >> num1;
+	
+	cout << "\n Number 2: ";
+		 cin >> num2;
+
+		myDiff = num1 - num2;
+
+	cout << fixed << setprecision(2)
+		 << "\n The Difference of " << num1 << " and " << num2
+		 << " is " << myDiff << endl << endl
+		 << num1 << " - " << num2 << " = " << myDiff << endl;
+}
+
+void slopeMe()
+{
+
+	double myX1		= 0.0;
+	double myY1		= 0.0;
+	double myX2		= 0.0;
+	double myY2		= 0.0;
+	double mySlope  = 0.0;
+
+	
+	cout << "\n Enter first X coordinate \n"
+		 << "\n X1: ";
+		 cin >> myX1;
+
+	cout << "\n Enter first Y coordinate \n"
+		 << "\n Y1: ";
+		 cin >> myY1;
+	
+	cout << "\n Enter second X coordinate \n"
+		 << "\n X2: ";
+		 cin >> myX2;
+	
+	cout << "\n Enter second Y coordinate \n"
+		 << "\n Y2: ";
+		 cin >> myY2;
+	
+	
+	mySlope = (myY1 - myY2)/(myX1 - myX2);
+
+	cout << fixed << setprecision(2)
+		 << "\n The Slope of (" << myX1 << "," << myY1 << ")" << " and (" << myX2 << "," << myY2 << ")" << endl
+		 << " is " << mySlope << "\n\n\n" << endl;
 }
