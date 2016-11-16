8f07dd440c4b7da1217728735acf614084dab833
i3-Arch <i3_arch@archlinux.info>
2016-11-16 01:52:49 -0500

    Changed variable name to something more suitable

--- Python/myprogram.py
+++ Python/myprogram.py
@@ -18,10 +18,10 @@ def MyBanner():
 def AskYourAge():
     AskMe = input("\n\n\033[1;37m  Would you like to enter your age? [\033[1;32mY\033[1;37m/\033[1;31mN\033[1;37m]:\033[1;m")
     if AskMe.lower() == 'y':
-        global TheVariable
+        global YourAge
         while True:
             try:
-                TheVariable = int(input("\n\n\033[1;37m  Enter in your age: \033[1;m"))
+                YourAge = int(input("\n\n\033[1;37m  Enter in your age: \033[1;m"))
                 break
             except (ValueError):
                 print("\n\n\n\033[1;41m  Oops! That was no valid number.\033[1;m\n\n")
@@ -34,19 +34,19 @@ def AskYourAge():
     print ()
 
 def CalcAge():
-    if TheVariable >= 13 and TheVariable <= 20:
+    if YourAge >= 13 and YourAge <= 20:
         print("\033[1;34m  You still have time on your side!\033[1;m")
-    elif TheVariable <= 0:
+    elif YourAge <= 0:
         print("\033[1;41m  Impossible, your age can't be less than or equal to 0\033[1;m")
-    elif TheVariable >= 1 and TheVariable <= 12:
+    elif YourAge >= 1 and YourAge <= 12:
         print("\033[1;34m  Yeah, and I'm a billionaire..\033[1;m")
-    elif TheVariable >= 21 and TheVariable <= 39:
+    elif YourAge >= 21 and YourAge <= 39:
         print("\033[1;34m  Well, seems like you're in your prime!\033[1;m")
-    elif TheVariable >= 40 and TheVariable <= 80:
+    elif YourAge >= 40 and YourAge <= 80:
         print("\033[1;34m  Meh, you're getting up there!\033[1;m")
-    elif TheVariable >= 81 and TheVariable <= 9000:
+    elif YourAge >= 81 and YourAge <= 9000:
         print("\033[1;35m  Yeah, I highly doubt that...\033[1;m")
-    elif TheVariable >= 9001:
+    elif YourAge >= 9001:
         print("\033[1;41m  ITS OVAR 9000!\033[1;m")
     else:
         print("\033[1;37m  Well, wtf have you entered....\033[1;m")
@@ -63,7 +63,7 @@ def AskYourName():
                 elif len(YourName) > 15:
                     print ("\033[1;31m \n\nError! \033[1;37mOnly 15 characters allowed!\033[1;m")
                 else:
-                    print ("\033[1;37m\n\n Hello\033[1;31m",YourName,"\033[1;37mwhom calims to be \033[1;31m",TheVariable,"\033[1;37mYears old.\033[1;m")
+                    print ("\033[1;37m\n\n Hello\033[1;31m",YourName,"\033[1;37mwhom calims to be \033[1;31m",YourAge,"\033[1;37mYears old.\033[1;m")
                 break
             except:
                 print ("\033[1;31mOh no! Something unexpected happened!\033[1;m")
