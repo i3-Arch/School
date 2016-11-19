2ce51ed04fc2315666d33198ab119897c2dd81b2
i3-Arch <i3_arch@archlinux.info>
2016-11-19 18:45:23 -0500

    Changed var/whitespace

--- Python/mymenu.py
+++ Python/mymenu.py
@@ -3,8 +3,9 @@
 #   -[ i3-Arch ]-
 #   Figured I'd play with python some
 
-def MyBanner():
-    print ("""\n\n\n\n\033[1;34m
+
+def my_banner():
+    print("""\n\n\n\n\033[1;34m
                 ######################################
                 ##                                  ##
                 ##              i3-Arch             ##
@@ -12,11 +13,11 @@ def MyBanner():
                 ##    https://github.com/i3-Arch    ##
                 ##                                  ##
                 ######################################\n\n\n\n\033[1;m""")
-    print ()
+    print()
 
-def MyMenu():
-    YourResp = 0
-    while YourResp != 3:
+def my_menu():
+    your_resp = 0
+    while your_resp != 3:
         try:
             print("""\033[1;31m              
                      [=========[\033[1;37mMENU\033[1;31m]=========] 
@@ -24,24 +25,24 @@ def MyMenu():
                      \033[1;31m||\033[1;34m  |2|\033[1;37m Something else\033[1;31m  ||
                      \033[1;31m||\033[1;34m  |3|\033[1;37m Exit       \033[1;31m     ||
                      [========================]\033[1;m""")
-            YourResp = int(input("\n\n\033[1;34m ==> \033[1;37m Enter your choice: \033[1;m")) 
+            your_resp = int(input("\n\n\033[1;34m ==> \033[1;37m Enter your choice: \033[1;m")) 
             break
-        except (ValueError):
+        except(ValueError):
             print("\n\n\n\033[1;31m Oops! You didn't enter a valid choice. Try again.\n\n\n\033[1;m")
 
-    if YourResp == 1:
-        print ("\033[1;37m \n\nHey, wassup breh...\n\n\033[1;m")
-        MyMenu()
-    elif YourResp == 2:
-        print ("\033[1;37m \n\n==> Also this...\n\n\033[1;m")
-        MyMenu()
-    elif YourResp == 3:
-        print ("\033[1;34m\n\n See you later!\033[1;m")
+    if your_resp == 1:
+        print("\033[1;37m \n\nHey, wassup breh...\n\n\033[1;m")
+        my_menu()
+    elif your_resp == 2:
+        print("\033[1;37m \n\n==> Also this...\n\n\033[1;m")
+        my_menu()
+    elif your_resp == 3:
+        print("\033[1;34m\n\n See you later!\033[1;m")
     else:
-        print ("\033[1;41m\n\n\n Sorry, I didn't understand your choice...\n\n\n\033[1;m")
-        print ("\033[1;41m\n\n Please try again.\n\n\n\033[1;m")
-        MyMenu()
-    print ()
+        print("\033[1;41m\n\n\n Sorry, I didn't understand your choice...\n\n\n\033[1;m")
+        print("\033[1;41m\n\n Please try again.\n\n\n\033[1;m")
+        my_menu()
+    print()
 
-MyBanner()
-MyMenu()
+my_banner()
+my_menu()
